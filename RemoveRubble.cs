d4cfec8839094e2b5ef9e6bee2caa788ed144dcc
GalaxyBlitz <akbaksh02@hotmail.com>
2017-09-04 19:01:43 +0100

    Create RemoveRubble.cs

---
+++ RemoveRubble.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+using System.Collections;
+
+public class RemoveRubble : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision col)
+    {
+        Destroy(col.gameObject);
+    }
+}
